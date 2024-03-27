f48fd3343343149b0329b730fa7c7fde82fc182e
sogancarmen1 <sogancarmen1@gmail.com>
2024-03-27 18:20:24 +0100

    quelques modifications dans les formulaires

--- src/components/FormPageConnexion.vue
+++ src/components/FormPageConnexion.vue
@@ -16,7 +16,6 @@ export default {
         else {
             this.showError = false;
         }
-        console.log("Value of show error : ", this.showError);
     }
   },
 }
@@ -24,7 +23,6 @@ export default {
 
 
 <template>
-  <RouterView></RouterView>
     <!--
         Faire la vérification au niveau de l'email (afficher le message en bas), faire le voir au niveau du password, 
         Vérifier si les données entré au niveau du password respect un certain regex? (Faire le message pour ça)
@@ -47,7 +45,7 @@ export default {
                             <p class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">Password</p>
                             <input v-model="password" placeholder="Enter your password" type="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"/>
                         </div>
-                        <div v-if="showError" class="text-sm text-red-700">Email or password invalid</div>
+                        <div v-if="showError" class="text-sm text-red-700 text-center py-2">Email or password invalid</div>
                         <router-link v-if="showError" to="/home"></router-link>
                         <div class="py-5">
                             <button @click="onSubmit" class="mx-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
--- src/components/FormPageInscription.vue
+++ src/components/FormPageInscription.vue
@@ -1,17 +1,46 @@
-<script setup lang="ts">
-const nameValue = "";
-const emailValue = "";
-const password = "";
-const confirmPassword = "";
-function onSubmit() {}
+<script>
+export default {
+  data() {
+    return {
+      nameValue: "",
+      emailValue: "",
+      password: "",
+      confirmPassword: "",
+      isEqual: false,
+      isNotCorrectEmail: false,
+      emailRegex: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      isEmpty: false
+    }
+  },
+
+  methods: {
+    onSubmit() {
+        if(this.password != this.confirmPassword) {
+            this.isEqual = true;
+        }
+        else {
+            this.isEqual = false;
+        }
+        if(this.emailRegex.test(this.emailValue) == false && this.emailValue != "") {
+            this.isNotCorrectEmail = true;
+        }
+        else {
+            this.isNotCorrectEmail = false;
+        }
+        if(this.nameValue == "" || this.emailValue == "" || this.password == "" || this.confirmPassword == "") {
+            this.isEmpty = true;
+        }
+        else {
+            this.isEmpty = false;
+        }
+        // console.log("Value of show error : ", this.isCorrectEmail);
+    }
+  },
+}
 </script>
 
 <template>
-    <RouterView></RouterView>
     <!--
-        Faire la vérification au niveau de l'email (afficher le message en bas), faire le voir au niveau du password, 
-        Vérifier si les données entré au niveau du password respect un certain regex? (Faire le message pour ça)
-        Vérifier si les données entré au niveau du password sont correctes? (Faire le message pour ça)
         Faire les vues au niveau des password
     -->
     <div class="flex items-center justify-center h-screen">
@@ -28,6 +57,7 @@ function onSubmit() {}
                     <div>
                         <p class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">Email</p>
                         <input v-model="emailValue" placeholder="Enter your email" type="email" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"/>
+                        <div v-if="isNotCorrectEmail" class="text-center text-sm text-red-500">incorrect email</div>
                     </div>
                     <div>
                         <p class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">Password</p>
@@ -36,18 +66,15 @@ function onSubmit() {}
                     <div>
                         <p class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">Confirm Password</p>
                         <input v-model="confirmPassword" placeholder="Confirm password" type="password" class="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"/>
-                    </div>
-                    <div class="space-x-20">
-                        <!-- <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 text-center px-14" href="#">
-                            you have account?
-                        </a> -->
+                        <div v-if="isEqual" class="text-center text-red-600 text-sm">password don't match</div>
+                        <div v-if="isEmpty" class="text-center text-red-600">the fiels shouldn't empty</div>
                     </div>
                     <div class="flex items-center mb-4 py-3">
                         <input id="default-checkbox" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
                         <label for="default-checkbox" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I accept <a href="#" class="underline text-blue-500">terms and conditions</a></label>
                     </div>
                     <div class="py-5">
-                        <button @click="onSubmit" class="mx-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+                        <button @click="onSubmit()" class="mx-20 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
                             Sign up
                         </button>
                     </div>
